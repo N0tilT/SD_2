abafceda237d09f6cb5219f174b5346d6fb09b18
Timofey Latypov <90103780+N0tilT@users.noreply.github.com>
2025-09-09 10:14:57 +0300

    Update main.py

--- DB/SQLITE/main.py
+++ DB/SQLITE/main.py
@@ -3,10 +3,23 @@ from flight import Flight
 
 service = FlightService("flights.db")
 
-service.create_flight(Flight(0,137,"137"))
-service.create_flight(Flight(0,371,"227"))
-service.create_flight(Flight(0,731,"227"))
-service.create_flight(Flight(0,111,"137"))
-service.create_flight(Flight(0,333,"227"))
+print("ADD 3")
+print(service.create_flight(Flight(0,111,"111")))
+print(service.create_flight(Flight(0,222,"222")))
+print(service.create_flight(Flight(0,333,"333")))
+print("GET ALL")
+print(service.get_all())
+
+print("UPDATE 2")
+print(service.update_flight(Flight(2,222,"hello")))
+print(service.get_by_id(2))
+
+print("DELETE 3")
+print(service.delete_flight(3))
+print(service.get_all())
+
+print("DELETE ALL")
+print(service.delete_flight(1))
+print(service.delete_flight(2))
 
 print(service.get_all())
