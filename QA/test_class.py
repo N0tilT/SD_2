e0fd6ebc24df1c9919a51a7ce782e39a7c1bbb94
Timofey Latypov <90103780+N0tilT@users.noreply.github.com>
2025-09-26 09:29:16 +0300

    tests

---
+++ QA/test_class.py
@@ -0,0 +1,20 @@
+class TestClass:
+    def test_one(self):
+        x = "this"
+        assert "h" in x
+
+    def test_two(self):
+        x = "check"
+        assert x == "check"
+
+    def test_three(self):
+        x = "check"
+        assert x == "check"
+
+    def test_four(self):
+        x = "check"
+        assert x == "check"
+
+    def test_five(self):
+        x = "check"
+        assert x == "check"
---
+++ QA/test_class2.py
@@ -0,0 +1,20 @@
+class TestClass2:
+    def test_one(self):
+        x = "this"
+        assert "h" in x
+
+    def test_two(self):
+        x = "check"
+        assert x == "check"
+
+    def test_three(self):
+        x = "check"
+        assert x == "check"
+
+    def test_four(self):
+        x = "check"
+        assert x == "check"
+
+    def test_five(self):
+        x = "check"
+        assert x == "check"
