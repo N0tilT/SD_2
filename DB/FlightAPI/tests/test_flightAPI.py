4467eabf02c75d57e8e690c500949af198c98d32
Timofey Latypov <90103780+N0tilT@users.noreply.github.com>
2025-09-26 11:47:15 +0300

    Update test_flightAPI.py

--- DB/FlightAPI/tests/test_flightAPI.py
+++ DB/FlightAPI/tests/test_flightAPI.py
@@ -12,7 +12,7 @@ class TestFlightAPI:
             "price":1000
         }]
         #Act
-        response_post = requests.post("localhost:8080/flights",data=data_in)
-        response_get = requests.get("localhost:8080/flights")
+        response_post = requests.post("http://localhost:8080/flights",data=data_in)
+        response_get = requests.get("http://localhost:8080/flights")
         #Assert
         assert data_expect==response_get.json()
